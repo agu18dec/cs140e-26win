9a64f949cd1e6d7f5b9f55fe8b2ec687dc6e14e3
Agam Bhatia <agam2026@stanford.edu>
2026-01-15 01:13:20 -0800

    lab1 complete

--- labs/1-trusting-trust/code/step1/quine-gen.c
+++ labs/1-trusting-trust/code/step1/quine-gen.c
@@ -5,5 +5,17 @@
 
 int main(void) { 
     // put your code here.
+    int c = 0;
+    int i = 0;
+    char prog[100000];
+    while ((c = getchar()) != EOF) {
+        prog[i++] = c;
+    }
+    prog[i] = 0;
+    printf("char prog[] = {\n");
+	for(i = 0; prog[i]; i++)
+		printf("\t%d,%c", prog[i], (i+1)%8==0 ? '\n' : ' ');
+    printf("0 };\n");
+    printf("%s", prog);
 	return 0;
 }
