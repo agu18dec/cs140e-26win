1bba26109e504bdfd64895b04f3ab01e896dbc86
Agam Bhatia <agam2026@stanford.edu>
2026-02-11 22:58:15 -0800

    lab 9 done

---
+++ labs/9-mailbox/0-measure/objs/l1/l2/l3/weird-result.d
@@ -0,0 +1,19 @@
+objs/l1/l2/l3//weird-result.o: weird-result.c \
+ /Users/agam/cs140e-26win//libpi/include/rpi.h \
+ /Users/agam/cs140e-26win//libpi/libc/demand.h \
+ /Users/agam/cs140e-26win//libpi/include/gpio.h \
+ /Users/agam/cs140e-26win//libpi/include/rpi-constants.h \
+ /Users/agam/cs140e-26win//libpi/include/your-prototypes.h \
+ /Users/agam/cs140e-26win//libpi/include/cycle-count.h
+
+/Users/agam/cs140e-26win//libpi/include/rpi.h:
+
+/Users/agam/cs140e-26win//libpi/libc/demand.h:
+
+/Users/agam/cs140e-26win//libpi/include/gpio.h:
+
+/Users/agam/cs140e-26win//libpi/include/rpi-constants.h:
+
+/Users/agam/cs140e-26win//libpi/include/your-prototypes.h:
+
+/Users/agam/cs140e-26win//libpi/include/cycle-count.h:
---
+++ labs/9-mailbox/0-sha-example/objs/l1/l2/l3/sha-test.d
@@ -0,0 +1,21 @@
+objs/l1/l2/l3//sha-test.o: sha-test.c \
+ /Users/agam/cs140e-26win//libpi/include/rpi.h \
+ /Users/agam/cs140e-26win//libpi/libc/demand.h \
+ /Users/agam/cs140e-26win//libpi/include/gpio.h \
+ /Users/agam/cs140e-26win//libpi/include/rpi-constants.h \
+ /Users/agam/cs140e-26win//libpi/include/your-prototypes.h sha256-fixed.h \
+ sha256-inner-loop.h
+
+/Users/agam/cs140e-26win//libpi/include/rpi.h:
+
+/Users/agam/cs140e-26win//libpi/libc/demand.h:
+
+/Users/agam/cs140e-26win//libpi/include/gpio.h:
+
+/Users/agam/cs140e-26win//libpi/include/rpi-constants.h:
+
+/Users/agam/cs140e-26win//libpi/include/your-prototypes.h:
+
+sha256-fixed.h:
+
+sha256-inner-loop.h:
