159d95a801649ef32238ff50ffdafff9d17f94e9
Agam Bhatia <agam2026@stanford.edu>
2026-02-26 18:39:09 -0800

    lab 13 complete

---
+++ labs/13-pinned-vm/code/objs/l1/l2/l3/tests/1-test-basic-tutorial.d
@@ -0,0 +1,52 @@
+objs/l1/l2/l3//tests/1-test-basic-tutorial.o: \
+ tests/1-test-basic-tutorial.c \
+ /Users/agam/cs140e-26win//libpi/include/rpi.h \
+ /Users/agam/cs140e-26win//libpi/libc/demand.h \
+ /Users/agam/cs140e-26win//libpi/include/gpio.h \
+ /Users/agam/cs140e-26win//libpi/include/rpi-constants.h \
+ /Users/agam/cs140e-26win//libpi/include/your-prototypes.h pinned-vm.h \
+ mmu.h armv6-cp15.h /Users/agam/cs140e-26win//libpi/libc/bit-support.h \
+ mem-attr.h /Users/agam/cs140e-26win//libpi/include/full-except.h \
+ /Users/agam/cs140e-26win//libpi/src/vector-base.h \
+ /Users/agam/cs140e-26win//libpi/include/switchto.h \
+ /Users/agam/cs140e-26win//libpi/include/cpsr-util.h \
+ /Users/agam/cs140e-26win//libpi/include/rpi-interrupts.h \
+ /Users/agam/cs140e-26win//libpi/include/rpi-inline-asm.h \
+ /Users/agam/cs140e-26win//libpi/include/memmap.h \
+ /Users/agam/cs140e-26win//libpi/include/asm-helpers.h
+
+/Users/agam/cs140e-26win//libpi/include/rpi.h:
+
+/Users/agam/cs140e-26win//libpi/libc/demand.h:
+
+/Users/agam/cs140e-26win//libpi/include/gpio.h:
+
+/Users/agam/cs140e-26win//libpi/include/rpi-constants.h:
+
+/Users/agam/cs140e-26win//libpi/include/your-prototypes.h:
+
+pinned-vm.h:
+
+mmu.h:
+
+armv6-cp15.h:
+
+/Users/agam/cs140e-26win//libpi/libc/bit-support.h:
+
+mem-attr.h:
+
+/Users/agam/cs140e-26win//libpi/include/full-except.h:
+
+/Users/agam/cs140e-26win//libpi/src/vector-base.h:
+
+/Users/agam/cs140e-26win//libpi/include/switchto.h:
+
+/Users/agam/cs140e-26win//libpi/include/cpsr-util.h:
+
+/Users/agam/cs140e-26win//libpi/include/rpi-interrupts.h:
+
+/Users/agam/cs140e-26win//libpi/include/rpi-inline-asm.h:
+
+/Users/agam/cs140e-26win//libpi/include/memmap.h:
+
+/Users/agam/cs140e-26win//libpi/include/asm-helpers.h:
---
+++ labs/13-pinned-vm/code/objs/l1/l2/l3/tests/1-test-one-addr.d
@@ -0,0 +1,53 @@
+objs/l1/l2/l3//tests/1-test-one-addr.o: tests/1-test-one-addr.c \
+ /Users/agam/cs140e-26win//libpi/include/rpi.h \
+ /Users/agam/cs140e-26win//libpi/libc/demand.h \
+ /Users/agam/cs140e-26win//libpi/include/gpio.h \
+ /Users/agam/cs140e-26win//libpi/include/rpi-constants.h \
+ /Users/agam/cs140e-26win//libpi/include/your-prototypes.h \
+ /Users/agam/cs140e-26win//libpi/include/full-except.h \
+ /Users/agam/cs140e-26win//libpi/src/vector-base.h \
+ /Users/agam/cs140e-26win//libpi/libc/bit-support.h \
+ /Users/agam/cs140e-26win//libpi/include/switchto.h \
+ /Users/agam/cs140e-26win//libpi/include/cpsr-util.h \
+ /Users/agam/cs140e-26win//libpi/include/rpi-interrupts.h \
+ /Users/agam/cs140e-26win//libpi/include/rpi-inline-asm.h pinned-vm.h \
+ mmu.h armv6-cp15.h mem-attr.h mmu.h memmap-default.h \
+ /Users/agam/cs140e-26win//libpi/include/memmap.h
+
+/Users/agam/cs140e-26win//libpi/include/rpi.h:
+
+/Users/agam/cs140e-26win//libpi/libc/demand.h:
+
+/Users/agam/cs140e-26win//libpi/include/gpio.h:
+
+/Users/agam/cs140e-26win//libpi/include/rpi-constants.h:
+
+/Users/agam/cs140e-26win//libpi/include/your-prototypes.h:
+
+/Users/agam/cs140e-26win//libpi/include/full-except.h:
+
+/Users/agam/cs140e-26win//libpi/src/vector-base.h:
+
+/Users/agam/cs140e-26win//libpi/libc/bit-support.h:
+
+/Users/agam/cs140e-26win//libpi/include/switchto.h:
+
+/Users/agam/cs140e-26win//libpi/include/cpsr-util.h:
+
+/Users/agam/cs140e-26win//libpi/include/rpi-interrupts.h:
+
+/Users/agam/cs140e-26win//libpi/include/rpi-inline-asm.h:
+
+pinned-vm.h:
+
+mmu.h:
+
+armv6-cp15.h:
+
+mem-attr.h:
+
+mmu.h:
+
+memmap-default.h:
+
+/Users/agam/cs140e-26win//libpi/include/memmap.h:
